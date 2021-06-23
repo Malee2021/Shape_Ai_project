7b0822ace6f1016bae49c92047936513e3b4da62
Malee2021 <86332220+Malee2021@users.noreply.github.com>
2021-06-23 11:56:19 +0530

    Add files via upload

---
+++ shape_ai_project.py
@@ -0,0 +1,35 @@
+import requests
+from datetime import datetime
+
+api_key='dedf33fa84ae217d90f0caeb2d9678e4'
+location=input("Enter the city name:")
+
+complete_api_link="https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link=requests.get(complete_api_link)
+api_data=api_link.json()
+
+temp_city=((api_data['main']['temp'])-273.15)
+
+weather_desc=api_data['weather'][0]['description']
+
+hmdt=api_data['main']['humidity']
+
+wind_spd=api_data['wind']['speed']
+
+date_time=datetime.now().strftime("%d %b %Y|%I:%M:%S %p")
+
+print("---------------------------------------------------")
+
+("weather states for-{}||{}".format(location.upper(),date_time))
+           
+print("---------------------------------------------------")
+
+print("current temperature is:{:.2f} deg C".format(temp_city))
+print("current weather desc:",weather_desc)
+print("current humidity:",hmdt, '%')
+print("current wind speed:",wind_spd,'kmph')
+
+
+
+           
+
